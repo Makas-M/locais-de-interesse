65493346b5277893b2698b237a1a501bd4ce15b4
Antonio Macaringue <110739274+Makas-M@users.noreply.github.com>
2023-02-08 16:06:37 +0200

    separacao do codigo

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
+var map = L.map('map').setView([-25.92253, 32.61006], 17);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}
+	).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([32.61194444444445,25.91777777777778]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+	L.marker([-25.92253,32.61006 ]).addTo(map)
+    .bindPopup('Minha Casa, +258 84 041 0143')
     .openPopup();
